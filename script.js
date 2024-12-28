7d6b072944b5c8090a759294068013b9c0bba6e7
sudosign <imodulo.8@gmail.com>
2024-12-28 16:45:53 -0500

    basics

---
+++ script.js
@@ -0,0 +1,30 @@
+function add(x,y){
+    return x + y;
+}
+
+function subtract(x,y){
+    return x - y;
+}
+
+function multiply(x,y){
+    return x * y;
+}
+
+function divide(x,y){
+    return x / y;
+}
+
+let x,operator,y;
+
+function operator(operator,x,y){
+    switch(operator){
+        case "+":
+            return add(x,y);
+        case "-":
+            return subtract(x,y);
+        case "*":
+            return multiply(x,y);
+        case "/":
+            return divide(x,y);
+    }
+}
